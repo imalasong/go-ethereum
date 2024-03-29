195abf1751ebb9109c7d90e58ba5808a81635d39
imalasong <2879499479@qq.com>
2024-03-29 10:37:01 +0800

    feat: add test

---
+++ study/t1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Usb interface {
+	Say()
+}
+type Stu struct {
+}
+
+func (this *Stu) Say() {
+	fmt.Println("Say()")
+}
+func main() {
+	// 错误！ 会报 Stu类型没有实现Usb接口 ,
+	// 如果希望通过编译,  var u Usb = &stu
+	u := Stu{}
+	u.Say()
+	fmt.Println("here", u)
+}
